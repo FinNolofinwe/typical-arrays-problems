71f507de927bd48c116dd27bf4adb1d3667232fb
FinNolofinwe <nolofinwe1881@gmail.com>
2021-03-09 22:49:26 +0300

    feat: add final solution to the task

--- src/index.js
+++ src/index.js
@@ -1,29 +1,31 @@
 
 exports.min = function min (array) {
-  if (!array.length) { 
-  let arr = array.sort((a, b) => a - b);
+  if (array && (array.length > 0)) {
+    let arr = array.sort((a, b) => a - b);
   return arr[0];
   } else {
-  return 0;
-}
+    return 0
+  }
 }
 
+
 exports.max = function max (array) {
-  if (!array.length) { 
+  if (array && (array.length > 0)) {
     let arr = array.sort((a, b) => a - b).reverse();
   return arr[0];
   } else {
-  return 0;
-}
+    return 0;
+  }
 }
 
+
 exports.avg = function avg (array) {
-  if (!array.length) {
-    let sum = 0;
+  if (array && (array.length > 0)) {
+    var sum = 0;
     for (i = 0; i <= array.length - 1; i++) {
-      sum = i++;
-      return sum / array.length;
+      sum = sum + array[i];    
     }
+    return sum / array.length;
   } else {
     return 0;
   }
