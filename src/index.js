256e2e94e8c471e038ad6ccad44e5242cf1c1d22
FinNolofinwe <nolofinwe1881@gmail.com>
2021-03-09 21:28:50 +0300

    feat: add draft solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 
 exports.min = function min (array) {
+  if (!array.length) { 
+  let arr = array.sort((a, b) => a - b);
+  return arr[0];
+  } else {
   return 0;
+}
 }
 
 exports.max = function max (array) {
+  if (!array.length) { 
+    let arr = array.sort((a, b) => a - b).reverse();
+  return arr[0];
+  } else {
   return 0;
+}
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (!array.length) {
+    let sum = 0;
+    for (i = 0; i <= array.length - 1; i++) {
+      sum = i++;
+      return sum / array.length;
+    }
+  } else {
+    return 0;
+  }
 }
+
+
